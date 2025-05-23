c0bf7bd93ca1c9d15f70f325cbbd40173e996106
Aarav Mehta <aaravmehta1207@gmail.com>
2025-05-22 23:42:08 -0400

    add new classes to single GPU for decoder/encoder

--- src/model/single_gpu_model.py
+++ src/model/single_gpu_model.py
@@ -2,8 +2,18 @@ import torch
 from torch import nn
 
 class TifUNET(nn.Module):
-    def __init__(self, pixel_height, pixel_width, num_channels):
-        super().__init__(pixel_height, pixel_width, num_channels)
+    def __init__(self, pixel_height, pixel_width, num_channels, batch_size):
+        super().__init__()
+        self.pixel_height = pixel_height
+        self.pixel_width = pixel_width
+        self.num_channels = num_channels
+        self.batch_size = batch_size
 
     def forward(self, input):
         pass
+
+class Decoder(nn.Module):
+    pass
+
+class Encoder(nn.Module):
+    pass
